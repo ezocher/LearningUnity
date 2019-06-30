8c32baf769a2b76075897c569ab071c6ecbb2405
Eric Zocher <ezocher@users.noreply.github.com>
2019-06-29 17:05:15 -0700

    Prefab instantiation working for some visuals

---
+++ Sandbox/Assets/Scripts/LaneRunner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneRunner : MonoBehaviour
+{
+    public float laneClosestDistanceMeters = 1f;
+    public float laneFarthestDistanceMeters = 5f;
+    public int laneNumberOfMusicClips = 3;
+
+    private int laneIndex;  // Lane Index = Lane number - 1
+                            // Lane numbers are 1..N and Lane indexes are 0..N-1
+
+    void Start()
+    {
+        // This GameObject's name is set by its creator/parent in Awake(), so it's not guaranteed to have been done until all Awake()s are finished
+        int laneNumber = int.Parse(this.name.Substring(this.name.Length - 1, 1)); // Use the last character of the GameObject's name as its number
+        laneIndex = laneNumber - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Sandbox/Assets/Scripts/LanesCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanesCreator : MonoBehaviour
+{
+    public int numberOfLanes = 4;
+    public float laneSeparationAngleDegrees = 20f;
+    public GameObject lanePrefab;
+
+    const string laneNamePrefix = "Lane";
+
+    void Awake()
+    {
+        float laneAngleY = -(laneSeparationAngleDegrees * (numberOfLanes - 1)) / 2;
+
+        for (int laneNumber = 1; laneNumber <= numberOfLanes; laneNumber++)
+        {
+            Quaternion laneYRotation = Quaternion.Euler(0, laneAngleY, 0);
+            laneAngleY += laneSeparationAngleDegrees;
+
+            GameObject newLane = Instantiate(lanePrefab, Vector3.zero, laneYRotation);
+            newLane.transform.parent = this.transform;
+            newLane.transform.localPosition = Vector3.zero;  // Reset position
+            newLane.name = laneNamePrefix + laneNumber.ToString();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
